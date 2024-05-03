55f04e7d6d657f885c3e0e0fe7bcb1f4835cea80
Afshin Ghezeli <a.g.afshin@gmail.com>
2024-05-03 11:00:00 +0430

    update products route

--- src/app/api/v1/products/route.ts
+++ src/app/api/v1/products/route.ts
@@ -1,7 +1,8 @@
 import { getProductsWithBlurHash } from "@/services/productService";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request: Request) {
+  const req = request;
   try {
     const productsWithPlaceholders = await getProductsWithBlurHash();
     return NextResponse.json(productsWithPlaceholders);
