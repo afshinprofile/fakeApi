6c09a9d7776e5e154c051f63ccd1011884ef7c09
Afshin Ghezeli <a.g.afshin@gmail.com>
2024-05-04 11:00:00 +0430

    refacort products route to handle CORS

--- src/app/api/v1/products/route.ts
+++ src/app/api/v1/products/route.ts
@@ -4,12 +4,34 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     const productsWithPlaceholders = await getProductsWithBlurHash();
-    return NextResponse.json(productsWithPlaceholders);
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Failed to fetch products: " + error },
-      { status: 500 }
+    const response = new NextResponse(
+      JSON.stringify(productsWithPlaceholders),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET", 
+          "Access-Control-Allow-Headers": "Content-Type", 
+          "Access-Control-Allow-Credentials": "true", 
+        },
+      }
     );
+
+    return response;
+  } catch (error) {
+    const response = new NextResponse(
+      JSON.stringify({ error: `Failed to fetch products: ${error}` }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*", 
+        },
+      }
+    );
+
+    return response;
   }
 }
 
